9b8db4d42c8498930e3e8276720273c314d23c35
rgurk <rafaelgurk@gmail.com>
2020-12-15 20:00:17 -0300

    15/12/20

---
+++ recursividade_em_cauda.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int fatorialCauda(int valor){
+    return fatorialAux(valor, 1);
+}
+
+int fatorialAux(int n, int parcial){
+    if(n != 1){
+        return fatorialAux((n - 1), (parcial * n));
+    }else{
+        return parcial;
+    }
+}
+
+int main(){
+    int n, resultado;
+
+    printf("\n Digite um numero inteiro positivo : ");
+    scanf("%d", &n);
+    resultado = fatorialCauda(n);//fazendo a primeira chamada da função
+    
+    printf("\n Resultado do fatorial = %d", resultado);
+return 0;
+}
